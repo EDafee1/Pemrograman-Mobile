66f80545d2c57f1e670b3591f2a01f12f520222d
Muhammad Dafi Hisbullah <dafimuhammad1@gmail.com>
2020-10-05 13:24:20 +0700

    addstring

--- MyFirstExercise/app/src/main/java/com/example/myfirstexercise/MainActivity.java
+++ MyFirstExercise/app/src/main/java/com/example/myfirstexercise/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myfirstexercise;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView_title = findViewById(R.id.title);
+        textView_title.setPaintFlags(textView_title.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
     }
 }
